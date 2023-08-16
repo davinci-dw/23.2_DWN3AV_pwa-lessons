c17c37d90a519bef1ebfa25fec5f2db97a3c7b00
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-08-15 22:58:41 -0300

    refactor: add Person class

--- clase_01/objetos.js
+++ clase_01/objetos.js
@@ -1,20 +1,35 @@
-const sergio = {
-    firstName: "Sergio",
-    lastName: "Medina",
-    age: 30,
-    getFirstName: function() {
+class Person {
+    firstName= "";
+    lastName= "";
+    age= 0;
+    getFirstName() {
         return this.firstName;
-    },
-    setFirstName: function (newFirstName){
+    };
+    setFirstName(newFirstName){
         this.firstName = newFirstName;
-    },
-    getLastName: function () {
+    };
+    getLastName() {
         return this.lastName;
-    },
-    getName: function () {
+    };
+    setLastName(lastName){
+      this.lastName = lastName;
+    };
+    getAge() {
+        return this.age;
+    }
+    setAge(age) {
+        this.age = age;
+    }
+    getName() {
         return this.getFirstName() + " " + this.getLastName();
+    };
+    constructor(firstName, lastName, age) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAge(age);
     }
 }
 
-sergio.setFirstName("Sergio Daniel");
-console.log(sergio.getName());
+const sergio = new Person("Sergio Daniel", "Medina", 30);
+
+console.log(sergio.getName(), sergio.getAge());
