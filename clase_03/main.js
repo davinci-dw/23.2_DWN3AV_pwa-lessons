7f73318d9a9e25082ce3255c0103eddcb8373d97
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-08-29 22:55:01 -0300

    feat: generar series list

--- clase_03/main.js
+++ clase_03/main.js
@@ -2,4 +2,9 @@ const serieManager = new SeriesApp();
 
 serieManager.addSerie(new Serie("Spiderman", "1930"));
 serieManager.addSerie(new Serie("Capitán américa", "2023"));
+serieManager.addSerie(new Serie("Hulk", "2021"));
+serieManager.addSerie(new Serie("Hulk", "2021"));
 
+const content = document.querySelector('.content');
+
+content.innerHTML = serieManager.generateList();
--- clase_03/series-app.js
+++ clase_03/series-app.js
@@ -6,4 +6,12 @@ class SeriesApp {
     addSerie(serie) {
         this.series.push(serie);
     }
+    generateList() {
+        return this.getSeries().map(serie => `
+            <div class="card">
+                <h3>${serie.getTitle()}</h3>
+                <p>${serie.getYear()}</p>
+            </div>
+        `).join('')
+    }
 }
