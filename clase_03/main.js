e89a7e8241fa600b4fe2a91f6af3dfb2593ae42a
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-08-29 22:36:57 -0300

    feat: add Serie class

---
+++ clase_03/main.js
@@ -0,0 +1,4 @@
+const primeraSerie = new Serie();
+
+primeraSerie.setYear(2023);
+console.log(primeraSerie.getYear());
---
+++ clase_03/serie.js
@@ -0,0 +1,33 @@
+class Serie {
+    title = '';
+    year = '';
+    imageUrl = '';
+
+    constructor() {
+    }
+
+    setTitle(title) {
+        this.title = title;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    setYear(year) {
+        this.year = year;
+    }
+
+    getYear() {
+        return this.year;
+    }
+
+    setImageUrl(imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    getImageUrl() {
+        return this.imageUrl;
+    }
+
+}
