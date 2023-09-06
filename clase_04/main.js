e38877b75d7835e2d71145b679cf74ca04d2b517
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-09-05 22:44:41 -0300

    feat: generate informationServer string

--- clase_04/main.js
+++ clase_04/main.js
@@ -6,13 +6,27 @@ const seriesList = [
     'Dr House',
     'breaking bad'
 ]
+const capitalize = (word) => {
+    const lower = word.toLowerCase();
+    return word.charAt(0).toUpperCase() + lower.slice(1);
+}
+
+const generateParagraph = (string) => `<p>${string}</p>`;
+
+const createCard = (paragraph) => `<div>
+    <h1>Card title</h1>
+    ${paragraph}
+</div>`;
 
 const informationServer = new Promise((done) => {
     setTimeout(()=> {
         done(seriesList)
     },TIEMPO_RETRASO);
 });
+
 informationServer
-    .then(value => {
-        console.log(value);
-    })
+    .then(list => list.map(capitalize))
+    .then(list => list.map(generateParagraph))
+    .then(list => list.map(createCard))
+    .then(list => list.join("_"))
+    .then(lastValue => console.log(lastValue));
