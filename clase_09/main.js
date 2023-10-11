b88812140da3581dc1ff87ea78685b3d222f47cc
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-10-10 22:05:54 -0300

    feat: render pokemon list

--- clase_09/main.js
+++ clase_09/main.js
@@ -2,13 +2,27 @@ const app = new Vue({
         el: '#app',
         data: {
             titulo: 'Hola mundo con Vue',
-            frutas: [
-                {nombre: 'Pera', cantidad: 10},
-                {nombre: 'Manzana', cantidad: 0},
-                {nombre: 'Platano', cantidad: 11}
-            ]
+            pokemons: []
+        },
+        methods: {
+            verPokemon(url) {
+               fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    const {weight, sprites} = data;
+                    const {front_shiny} = sprites;
+                    console.log(`Nombre: ${front_shiny} - Peso: ${weight}`);
+                });
+            }
+        },
+        mounted() {
+            fetch("https://pokeapi.co/api/v2/pokemon/?limit=5&offset=0")
+                .then(response => response.json())
+                .then(data => {
+                    this.pokemons = data.results;
+                });
         }
     }
 );
 
-console.log(app);
+console.log("https://pokeapi.co/api/v2/pokemon/?limit=5&offset=0");
