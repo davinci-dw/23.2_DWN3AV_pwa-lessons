1188988950de8730932bd86d09f7ba476d28b306
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-14 22:45:03 -0300

    feat: add auth redirection

--- clase_12/main.js
+++ clase_12/main.js
@@ -1,4 +1,28 @@
-if("serviceWorker" in navigator) {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA6r-lPy-EClqFBBpbeJQ3kgWL3Mc3HP84",
+    authDomain: "thepokeapp-01.firebaseapp.com",
+    projectId: "thepokeapp-01",
+    storageBucket: "thepokeapp-01.appspot.com",
+    messagingSenderId: "1035814182424",
+    appId: "1:1035814182424:web:a246e5ab8ebf3cb3fa2d6d",
+    measurementId: "G-EGZVKW2518"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+if(false && "serviceWorker" in navigator) {
     navigator.serviceWorker.register("./sw.js")
     .then((registration) => {
         console.log("service worker registrado");
@@ -9,7 +33,7 @@ if("serviceWorker" in navigator) {
 }
 
 
-const app = new Vue({
+const application = new Vue({
         el: '#app',
         data: {
             estadoModal: false,
@@ -36,11 +60,24 @@ const app = new Vue({
             }
         },
         mounted() {
-            fetch("https://pokeapi.co/api/v2/pokemon/?limit=5&offset=0")
-                .then(response => response.json())
-                .then(data => {
-                    this.pokemons = data.results;
-                });
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    // User is signed in, see docs for a list of available properties
+                    // https://firebase.google.com/docs/reference/js/auth.user
+                    //const uid = user.uid;
+                    fetch("https://pokeapi.co/api/v2/pokemon/?limit=5&offset=0")
+                        .then(response => response.json())
+                        .then(data => {
+                            this.pokemons = data.results;
+                        });
+                    // ...
+                } else {
+                    window.location.replace("login");
+                }
+            });
+
+
+
         }
     }
 );
