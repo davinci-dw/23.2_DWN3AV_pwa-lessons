156fd542432aaf9253032858a8aa5e4b5dd43811
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-21 22:37:23 -0300

    feat: add firestore support

--- clase_13/main.js
+++ clase_13/main.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+import { getFirestore, onSnapshot, collection, query, where} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,6 +23,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 if(false && "serviceWorker" in navigator) {
     navigator.serviceWorker.register("./sw.js")
@@ -39,7 +42,8 @@ const application = new Vue({
             estadoModal: false,
             pokemonActual: null,
             titulo: 'Hola mundo con Vue',
-            pokemons: []
+            pokemons: [],
+            nombre_usuario: ''
         },
         methods: {
             mostrarModal() {
@@ -60,8 +64,24 @@ const application = new Vue({
             }
         },
         mounted() {
-            onAuthStateChanged(auth, (user) => {
+            onAuthStateChanged(auth, async (user) => {
                 if (user) {
+                    //const querySnapshot = await
+                        onSnapshot(
+                            query(
+                                collection(db, "usuarios"),
+                                where("uid", "==", user.uid)
+                            ), (querySnapshot) => {
+                            querySnapshot.forEach((doc, i) => {
+                                const id = doc.id;
+                                const data = doc.data();
+                                //console.log(`${id} => ${data.uid}`);
+                                this.nombre_usuario = data.nombre;
+                                console.log(i,`${id} => ${data.nombre}`);
+                            });
+
+                        })
+
                     // User is signed in, see docs for a list of available properties
                     // https://firebase.google.com/docs/reference/js/auth.user
                     //const uid = user.uid;
